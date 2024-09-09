65762883f60ea7f227856a955db3d5358ff41495
Burak <burakbayramnl@hotmail.com>
2024-09-09 14:31:19 +0200

    lambdas

--- src/main/java/nl/han/bewd/lambdas/Main.java
+++ src/main/java/nl/han/bewd/lambdas/Main.java
@@ -5,10 +5,6 @@ import nl.han.bewd.lambdas.reis.*;
 import java.util.Random;
 
 public class Main {
-    /*
-    Een Reis wordt gemaakt met een enkel vervoermiddel. De reistijd kan door middel van een simulatie van de
-    reis worden berekend. Hierbij kan onderscheid gemaakt worden of de reis tijdens de spits of buiten de spits is.
-     */
     public static void main(String[] args) {
         System.out.println("Buiten spits met fiets");
         Reis mijnReis = new Reis(false);
